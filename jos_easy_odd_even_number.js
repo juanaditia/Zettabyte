d194b14cf65e79cc232ffd833796c312c33156d1
Juan Frido Aditiya <62372749+juanaditia@users.noreply.github.com>
2022-07-15 17:03:49 +0700

    Add files via upload

---
+++ jos_easy_odd_even_number.js
@@ -0,0 +1,17 @@
+// Expected result : [[1, 3, 5, 7, 9], [2, 4, 6, 8, 10]]
+// Direction : Return nested array first is odd value and second is even array from the array number
+const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function result(number) {
+  // Your Code Here
+  for (let i = 0; i < number.length; i++) {
+    if (i % 2 == 0) {
+      z = [i];
+    } else {
+      y = [i];
+    }
+  }
+  number = [[z], [y]];
+}
+
+console.log(result(number));
