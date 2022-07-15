2f76a0d3d4d0d3280480e673aeca052d1088ead4
Juan Frido Aditiya <62372749+juanaditia@users.noreply.github.com>
2022-07-15 17:07:57 +0700

    second commit

---
+++ fir_hard_primeArray.js
@@ -0,0 +1,12 @@
+
+// Direction : For each value inside the array, get the next smallest prime number value greater than the input number, if input number is already prime return that input
+// For example: Input: 4, Expected: 5, Input: 14, Expected: 17, Input: 2, Expected: 2
+// Expected: [2, 5, 19, 23, 37, 89]
+const number = [2, 4, 18, 20, 35, 84];
+const answer = [];
+
+function result(num) {
+  // Your Code Here
+}
+
+console.log(result(answer));
